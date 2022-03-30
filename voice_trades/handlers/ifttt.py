925e6451b9df31f3e9cb6cefbfdb2b229d5aa825
Julian <77919700+jujuhere@users.noreply.github.com>
2022-03-30 15:22:11 +0200

    Update ifttt.py

--- voice_trades/handlers/ifttt.py
+++ voice_trades/handlers/ifttt.py
@@ -9,7 +9,7 @@ class RequestHandler:
     # todo: implement skeleton for the post request
     def post_data(self, value_1: str, value_2: str):
         notification = requests.post(
-            self.ifttt_url + endpoint, params = self.params)
+            self.ifttt_url + endpoint, params = {'value1':return_title,'value2':return_body}) # i think params = self.params doesn't make sense 
         return notification 
 
 class IFTTT(RequestHandler):
