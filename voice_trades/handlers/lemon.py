18a15eb1baa89f1ef882cbe13fd9485b44739275
Julian <77919700+jujuhere@users.noreply.github.com>
2022-03-30 15:00:45 +0200

    Update lemon.py

--- voice_trades/handlers/lemon.py
+++ voice_trades/handlers/lemon.py
@@ -9,12 +9,29 @@ class RequestHandler:
 
     # todo: implement; should provide a 'structure' for all subsequent post requests
     def post_data(self, endpoint: str, data: dict):
+        response = requests.post(
+            self.url_trading + endpoint, json.dumps(data), headers = self.headers)
+        return response.json()
 
 class LemonMarketsAPI(RequestHandler):
     # todo: using the post_data method, place an order
     # hint: return order ID
     def place_order(self, isin: str, expires_at: str, side: str, quantity: str, venue: str):
+        venue = os.environ.get("MIC")
+        
+        order_details = {
+            "isin" : isin, 
+            "expires_at" : expires_at,
+            "side": side,
+            "quantity" : quantity,
+            "venue" : venue
+        }
+        return self.post_data(f"orders/", order_details)
 
     # todo: using the post_data method, activate an order
     # hint: return response in json format
     def activate_order(self):
+        return self.post_data(
+            f"orders/{order_id}/activate/", {}
+        )
+        
