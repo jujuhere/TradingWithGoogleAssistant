4fe2bacfb52b0b1008bb2afec9ffdc7e7fa7dd9d
Julian <77919700+jujuhere@users.noreply.github.com>
2022-03-31 12:00:55 +0200

    Update ifttt.py

--- voice_trades/handlers/ifttt.py
+++ voice_trades/handlers/ifttt.py
@@ -9,12 +9,12 @@ class RequestHandler:
     # todo: implement skeleton for the post request
     def post_data(self, value_1: str, value_2: str):
         notification = requests.post(
-            self.ifttt_url + endpoint, params = {'value1':return_title,'value2':return_body}) # i think params = self.params doesn't make sense 
-        return notification 
+            self.ifttt_url, params = {'value1':value_1,'value2':value_2})  
+        return notification
 
 class IFTTT(RequestHandler):
     def send_notification(self, side: str, quantity: int, symbol: str):
         return_title = 'Lemon.markets ' + ('buy' if side == 'buy' else 'sell') + ' Order Submitted'
         return_body = 'Order to ' + side + ' ' + str(quantity) + ' shares of ' + symbol + ' submitted.'
 
-        post_data(return_title, return_body)
+        return self.post_data(return_title, return_body)
